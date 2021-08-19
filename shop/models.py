cb5f06271faf58fd71311dee50611cefe963271e
Mogrima <rebel.rs@yandex.ru>
2021-08-19 21:01:19 +0500

    в модели добавлено формирование ссылок и новая модель для статей. Прописаны новые пути в urls

--- shop/models.py
+++ shop/models.py
@@ -14,8 +14,8 @@ class Goods(models.Model):
     def __str__(self):
         return self.title
 
-    # def get_absolute_url(self):
-    #     return reverse('post', kwargs={'post_slug': self.slug})
+    def get_absolute_url(self):
+        return reverse('post', kwargs={'post_slug': self.slug})
 
     class Meta:
         verbose_name = 'Товары'
@@ -29,10 +29,31 @@ class Category(models.Model):
     def __str__(self):
         return self.name
 
-    # def get_absolute_url(self):
-    #     return reverse('category', kwargs={'cat_slug': self.slug})
+    def get_absolute_url(self):
+        return reverse('category', kwargs={'cat_slug': self.slug})
 
     class Meta:
         verbose_name = 'Категория'
         verbose_name_plural = 'Категории'
         ordering = ['id']
+
+class Article(models.Model):
+    title = models.CharField(max_length=255, verbose_name='Заголовок')
+    slug = models.SlugField(max_length=255, unique=True, db_index=True, verbose_name="URL")
+    content = models.TextField(blank=True, verbose_name="Текст статьи")
+    photo = models.ImageField(upload_to="photos/%Y/%m/%d/", verbose_name="Фото")
+    time_create = models.DateTimeField(auto_now_add=True)
+    time_update = models.DateTimeField(auto_now=True)
+    is_published = models.BooleanField(default=True)
+    cat = models.ForeignKey('Category', on_delete=models.PROTECT)
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse('article', kwargs={'article_slug': self.slug})
+
+    class Meta:
+        verbose_name = 'Заголовок'
+        verbose_name_plural = 'Заголовки'
+        ordering = ['time_create', 'title']
--- shop/urls.py
+++ shop/urls.py
@@ -4,6 +4,11 @@ from django.urls import path, re_path
 from .views import *
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', index),
+    path('', ShopHome.as_view(), name='home'),
+    path('about/', about, name='about'),
+    path('article/', ShowArticle.as_view(), name='article'),
+    path('login/', login, name='login'),
+    path('post/<slug:post_slug>/', ShowGoods.as_view(), name='post'),
+    path('category/<slug:cat_slug>/', GoodsCategory.as_view(), name='category'),
+    path('cats/<int:catid>/', categories),
 ]
