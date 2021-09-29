170ac83d364dd9889ea7a8d26f5e612cb4d222ad
Mogrima <rebel.rs@yandex.ru>
2021-09-29 07:58:04 +0500

    переписан функционал открытия закрытия меню через функцию и дата-атрибут

--- shop/static/shop/js/main.js
+++ shop/static/shop/js/main.js
@@ -1,40 +1,39 @@
-const navUser = document.querySelector('.user-menu');
-const UserToggle = document.querySelector('.user-menu__toggle');
-
-if (navUser != null) {
-  navUser.classList.remove('user-menu--nojs');
-}
-
-if (navUser != null) {
-  UserToggle.addEventListener('click', function () {
-    if (navUser.classList.contains('user-menu--closed')) {
-      navUser.classList.remove('user-menu--closed');
-      navUser.classList.add('user-menu--opened');
-    } else {
-      navUser.classList.add('user-menu--closed');
-      navUser.classList.remove('user-menu--opened');
-    }
-  });
-}
+const navUser = document.querySelectorAll('.user-menu');
+const UserToggle = document.querySelectorAll('.user-menu__toggle');
 
 const navMain = document.querySelector('.nav');
 const navToggle = document.querySelector('.nav__toggle');
 
 navMain.classList.remove('nav--nojs');
 
-navToggle.addEventListener('click', function () {
-  if (navMain.classList.contains('nav--closed')) {
-    navMain.classList.remove('nav--closed');
-    navMain.classList.add('nav--opened');
-  } else {
-    navMain.classList.add('nav--closed');
-    navMain.classList.remove('nav--opened');
-  }
-});
-
 const activeMenu = document.querySelectorAll('.nav__item--active');
 
 if (activeMenu[1]) {
   activeMenu[0].classList.remove('nav__item--active')
 }
+
+if (navUser != null) {
+  navUser.forEach(function(item) {
+    item.classList.remove('user-menu--nojs');
+  });
+}
+
+function toggleClass(parent, classA, classB) {
+  document.addEventListener('click', function (event) {
+    if (event.target.dataset.toggle != undefined) {
+      let currentNav = event.target.closest(parent);
+      
+      if (currentNav.classList.contains(classA)) {
+        currentNav.classList.remove(classA);
+        currentNav.classList.add(classB);
+      } else {
+        currentNav.classList.add(classA);
+        currentNav.classList.remove(classB);
+      }
+    }
+  });
+}
+
+toggleClass('nav', 'user-menu--closed', 'user-menu--opened');
+toggleClass('nav', 'nav--closed', 'nav--opened');
 
