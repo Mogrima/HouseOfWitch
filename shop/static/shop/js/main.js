d4bc924467d69e198588eeddf80b0a46e7b9d8e1
Mogrima <rebel.rs@yandex.ru>
2022-04-12 23:21:29 +0500

    данные в форме заказа сохраняются, если есть какая-то ошибка

--- shop/static/shop/js/main.js
+++ shop/static/shop/js/main.js
@@ -51,3 +51,33 @@ if(window.matchMedia('(max-width: 767px)').matches) {
       }
   }
 }
+
+const order_form = document.getElementById("form-order");
+let first_name = document.getElementById("id_first_name");
+let last_name = document.getElementById("id_last_name");
+let surname = document.getElementById("id_surname");
+let email = document.getElementById("email");
+let phone = document.getElementById("id_phone");
+let adress = document.getElementById("id_adress");
+
+const input = order_form.querySelectorAll("input");
+
+function isItemExist(name) {
+  return (name in localStorage)
+}
+
+first_name.value = (isItemExist('first_name')) ? localStorage.first_name : ''
+last_name.value = (isItemExist('last_name')) ? localStorage.last_name : ''
+surname.value = (isItemExist('surname')) ? localStorage.surname : ''
+email.value = (isItemExist('email')) ? localStorage.email : email.value
+phone.value = (isItemExist('phone')) ? localStorage.phone : ''
+adress.value = (isItemExist('adress')) ? localStorage.adress : ''
+
+order_form.addEventListener('submit', () => {
+  localStorage.first_name = first_name.value
+  localStorage.last_name = last_name.value
+  localStorage.surname = surname.value
+  localStorage.email = email.value
+  localStorage.phone = phone.value
+  localStorage.adress = adress.value
+})
