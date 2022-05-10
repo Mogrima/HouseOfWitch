f0cc45641bc389a7375606368e4ee15d75012c06
Mogrima <rebel.rs@yandex.ru>
2022-05-10 15:44:53 +0500

    стилизованы страницы 404, 500 и т.д.

--- houseWitch/views.py
+++ houseWitch/views.py
@@ -1,15 +1,14 @@
 from django.shortcuts import render
 
 
-
 def page_not_found_view(request, exception):
-    return render(request, 'shop/404.html', status=404)
+    return render(request, 'shop/404.html', {'title': '404 Page Not Found'}, status=404)
 
 def permission_denied_view(request, exception):
-    return render(request, 'shop/403.html', status=403)
+    return render(request, 'shop/403.html', {'title': '403 permission denaided'}, status=403)
 
 def bad_request_view(request, exception):
-    return render(request, 'shop/400.html', status=400)
+    return render(request, 'shop/400.html', {'title': '400 Bad request'}, status=400)
 
 def server_error_view(request):
-    return render(request, 'shop/500.html', status=500)
+    return render(request, 'shop/500.html', {'title': '500 Server Error'}, status=500)
