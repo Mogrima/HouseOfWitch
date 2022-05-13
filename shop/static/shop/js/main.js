e787ae1432ba04787708d730fb7b11b6600fb13a
Mogrima <rebel.rs@yandex.ru>
2022-05-13 19:56:03 +0500

    добавлена кнопка вверх при прокрутке страницы

--- shop/static/shop/js/main.js
+++ shop/static/shop/js/main.js
@@ -142,3 +142,31 @@ getCountGoods();
   }
 
 })();
+
+(function() {
+  'use strict';
+
+  function trackScroll() {
+    var scrolled = window.pageYOffset;
+    var coords = document.documentElement.clientHeight;
+
+    if (scrolled > coords) {
+      goTopBtn.classList.add('up_button-show');
+    }
+    if (scrolled < coords) {
+      goTopBtn.classList.remove('up_button-show');
+    }
+  }
+
+  function backToTop() {
+    if (window.pageYOffset > 0) {
+      window.scrollBy(0, -80);
+      setTimeout(backToTop, 0);
+    }
+  }
+
+  var goTopBtn = document.querySelector('.up_button');
+
+  window.addEventListener('scroll', trackScroll);
+  goTopBtn.addEventListener('click', backToTop);
+})();
