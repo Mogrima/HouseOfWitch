688b46378fc01855d54c1b3a3eb3f939450a709d
Mogrima <rebel.rs@yandex.ru>
2022-05-18 22:23:55 +0500

    добавлены шаблоны для страниц сброса пароля, в главном urls прописаны пути к этим шаблонам

--- houseWitch/urls.py
+++ houseWitch/urls.py
@@ -22,6 +22,7 @@ from shop.views import *
 
 urlpatterns = [
     path('receive/', admin.site.urls),
+    path('accounts/', include('django.contrib.auth.urls')),
     path('', include('shop.urls')),
 ]
 
